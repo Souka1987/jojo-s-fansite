05e95545a8b34c166dec2902141528fe453cb042
Souka1987 <soukainataattoumani@yahoo.fr>
2020-08-31 09:51:19 +0200

    init node

---
+++ api/router.js
@@ -0,0 +1,23 @@
+// Import
+const express = require('express'),
+    router = express.Router(),
+    path = require('path')
+
+// Controller
+const homeController = require('./controllers/homeController'),
+    articleController = require('./controllers/articleController'),
+    contactController = require('./controllers/contactController')
+
+// Home
+router.route('/')
+    .get(homeController.get)
+
+// Article
+router.route('/article')
+    .get(articleController.get)
+
+// Contact
+router.route('/contact')
+    .get(contactController.get)
+
+module.exports = router;
